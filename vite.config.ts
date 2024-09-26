6ca20e27a084d3321a535f91716739004b6a1a30
hiroshi-hexmount <hiroshi@hexmount.io>
2024-09-26 15:26:53 +0900

    update package

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    topLevelAwait({
-      // The export name of top-level await promise for each chunk module
-      promiseExportName: "__tla",
-      // The function to generate import names of top-level await promise in each chunk module
-      promiseImportName: i => `__tla_${i}`
-    }),
-    react(), 
-    nodePolyfills(), 
-    wasm()],
+  plugins: [react()],
   server: {
     host: true,
     port: 5173,
