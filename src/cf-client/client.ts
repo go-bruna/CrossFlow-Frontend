1b5b740bd31c804f06ca27daa80637051e270d1e
kenta-hexmount <kenta@hexmount.io>
2024-10-28 08:23:05 +0000

    chore: add cf-client and CFNProvider

---
+++ src/cf-client/client.ts
@@ -0,0 +1,39 @@
+import { SigningStargateClient, StdFee } from "@cosmjs/stargate";
+import { Registry, OfflineSigner, EncodeObject } from "@cosmjs/proto-signing";
+import { MsgVote } from "./cosmos.gov/tx";
+
+const defaultFee = {
+  amount: [],
+  gas: "2000000",
+};
+
+interface SignAndBroadcastOptions {
+  fee: StdFee;
+  memo?: string;
+}
+
+const types = [
+  ["/cosmos.gov.v1.MsgVote", MsgVote],
+];
+
+export const registry = new Registry(<any>types);
+
+export const TxClient = async (wallet: OfflineSigner) => {
+  const client = await SigningStargateClient.connectWithSigner(
+    process.env.CROSSFLOW_TESTNET_RPC,
+    wallet,
+    { registry }
+  );
+  const { address } = (await wallet.getAccounts())[0];
+
+  return {
+    signAndBroadcast: (
+      msgs: EncodeObject[],
+      { fee, memo }: SignAndBroadcastOptions = { fee: defaultFee, memo: "" }
+    ) => client.signAndBroadcast(address, msgs, fee, memo),
+    msgVote: (data: MsgVote): EncodeObject => ({
+      typeUrl: "/cosmos.vote.v1.MsgVote",
+      value: MsgVote.fromPartial(data),
+    }),
+  };
+};
--- ts-client/cfprotocol.cfnconsensus/index.ts
+++
@@ -1,6 +0,0 @@
-import IgntModule from './module';
-import { txClient, registry } from './module';
-import { msgTypes } from './registry';
-
-export * from "./types";
-export { IgntModule, msgTypes, txClient, registry };
