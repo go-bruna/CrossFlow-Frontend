441e0b137ef35a9a1156a1f83d4292d9d7b35025
lyricalstring <lyricalstring@outlook.es>
2024-11-19 16:41:20 +0100

    ♻️ (borrow.base.tsx, useWeb3.tsx): refactor code for improved readability and maintainability
    The code has been refactored to enhance readability by organizing imports, improving variable naming conventions, and ensuring consistent formatting. This refactor aims to make the codebase easier to navigate and maintain, which is essential for future development and collaboration. Additionally, the logic for handling token approvals has been updated to include error handling for missing TSS public keys, ensuring a more robust user experience.

--- src/hooks/useWeb3.tsx
+++ src/hooks/useWeb3.tsx
@@ -3,91 +3,107 @@ import { ethers } from "ethers";
 import { useAccount } from "wagmi";
 import { type DecodedError, ErrorDecoder } from "ethers-decode-error";
 import { useEthersProvider, useEthersSigner } from "@/utils/ethersAdapter";
-import {
-  CHAIN_ID,
-  O_USDT_TOKEN,
-} from "@/constants";
+import { CHAIN_ID, O_USDT_TOKEN } from "@/constants";
 import { useToast } from "./useToast";
 import { fromWei, toWei } from "@/utils";
 import { useTxModalState } from "@/contexts/tx-modal";
 import { WARNING_MESSAGE } from "@/constants/message";
 import { MODAL_STATE } from "@/types/interfaces";
+import { getTssPublicKey } from "@/apis/pool";
 
 export const useSigningWeb3Client = () => {
-  const { address } = useAccount();
-  const provider = useEthersProvider({ chainId: CHAIN_ID });
+	const { address } = useAccount();
+	const provider = useEthersProvider({ chainId: CHAIN_ID });
 
-  const signer = useEthersSigner({ chainId: CHAIN_ID });
-  const { messageApi } = useToast();
-  const { setIsTxModal, updateModalState } = useTxModalState();
-  const errorDecoder = ErrorDecoder.create();
+	const signer = useEthersSigner({ chainId: CHAIN_ID });
+	const { messageApi } = useToast();
+	const { setIsTxModal, updateModalState } = useTxModalState();
+	const errorDecoder = ErrorDecoder.create();
 
-  const getTokenBalance = useCallback(async () => {
-    try {
-      const contract = new ethers.Contract(
-        O_USDT_TOKEN.address,
-        O_USDT_TOKEN.abi,
-        provider
-      );
-      const result = await contract.balanceOf(address);
-      return fromWei(result)
-    } catch (err) {
-      console.log(err);
-    }
-  }, [provider, signer, address]);
+	const getTokenBalance = useCallback(async () => {
+		try {
+			const contract = new ethers.Contract(
+				O_USDT_TOKEN.address,
+				O_USDT_TOKEN.abi,
+				provider,
+			);
+			const result = await contract.balanceOf(address);
+			return fromWei(result);
+		} catch (err) {
+			console.log(err);
+		}
+	}, [provider, signer, address]);
 
-  const approveUSDT = useCallback(async(amount: number) => {
-    try {
+	const approveUSDT = useCallback(
+		async (amount: number) => {
+			try {
+				updateModalState(MODAL_STATE.APPROVE, "Approving", " ", "", 2);
+				setIsTxModal(true);
 
-      updateModalState(MODAL_STATE.APPROVE, "Approving", " ", "", 2);
-      setIsTxModal(true);
+				const tssPublicKey = await getTssPublicKey();
+				if (!tssPublicKey) {
+					messageApi.Alert({
+						...WARNING_MESSAGE,
+						content: "No tss public key found",
+					});
+					return false;
+				}
 
-      let contract = new ethers.Contract(
-        O_USDT_TOKEN.address,
-        O_USDT_TOKEN.abi,
-        signer
-      );
+				let contract = new ethers.Contract(
+					tssPublicKey.tss_pubkey[0].ecdsa,
+					O_USDT_TOKEN.abi,
+					signer,
+				);
 
-      const tokenAmount = amount.toString();
-      const params: string[] = [O_USDT_TOKEN.address, toWei(tokenAmount)];
+				const tokenAmount = amount.toString();
+				// Replace this address by the address from tss_pubkey
+				const params: string[] = [O_USDT_TOKEN.address, toWei(tokenAmount)];
 
-      if (!contract) {
-        messageApi.Alert({ ...WARNING_MESSAGE, content: `Contract doesn't work for now, please try again.`})
-        return false
-      }
+				if (!contract) {
+					messageApi.Alert({
+						...WARNING_MESSAGE,
+						content: `Contract doesn't work for now, please try again.`,
+					});
+					return false;
+				}
 
-      let transaction = {
-        from: address,
-        to: O_USDT_TOKEN.address,
-        data: contract.interface.encodeFunctionData("approve", params),
-      };
+				let transaction = {
+					from: address,
+					to: O_USDT_TOKEN.address,
+					data: contract.interface.encodeFunctionData("approve", params),
+				};
 
-      await provider?.estimateGas(transaction);
-      let tx = await contract.approve(
-        O_USDT_TOKEN.address,
-        toWei(tokenAmount)
-      );
-      console.log("transaction of approve ==>", tx)
-      await tx.wait();
+				await provider?.estimateGas(transaction);
+				let tx = await contract.approve(
+					O_USDT_TOKEN.address,
+					toWei(tokenAmount),
+				);
+				console.log("transaction of approve ==>", tx);
+				await tx.wait();
 
-      updateModalState(
-        MODAL_STATE.CONFIRM,
-        "Confirm",
-        `${amount} USDT`,
-        "",
-        2
-      );
-      return true
-    } catch (err) {
-      console.log(err);
-      const error: DecodedError = await errorDecoder.decode(err);
-      messageApi.Alert({...WARNING_MESSAGE, content: error?.reason ?? undefined});
-      return false
-    }
-  }, [provider, signer, address]);
+				updateModalState(
+					MODAL_STATE.CONFIRM,
+					"Confirm",
+					`${amount} USDT`,
+					"",
+					2,
+				);
+				return true;
+			} catch (err) {
+				console.log(err);
+				const error: DecodedError = await errorDecoder.decode(err);
+				messageApi.Alert({
+					...WARNING_MESSAGE,
+					content: error?.reason ?? undefined,
+				});
+				return false;
+			}
+		},
+		[provider, signer, address],
+	);
 
-  return {
-    approveUSDT,
-    getTokenBalance
-  }
+	return {
+		approveUSDT,
+		getTokenBalance,
+	};
 };
