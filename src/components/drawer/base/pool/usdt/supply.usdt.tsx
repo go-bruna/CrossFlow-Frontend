3027c292a9707e7dc1dce36eda7bd1b5890c211f
hiroshi-hexmount <hiroshi@hexmount.io>
2024-11-25 22:34:21 +0900

    fix conflicts

--- src/components/drawer/base/pool/usdt/supply.usdt.tsx
+++ src/components/drawer/base/pool/usdt/supply.usdt.tsx
@@ -1,259 +1,258 @@
-import { useEffect, ChangeEvent, useState, useMemo } from "react";
-import Button from "@/components/button";
-import Table from "@/components/table";
-import Input from "@/components/input";
-import { AmountIcon } from "@/assets/icons/amount";
-import { twMerge } from "tailwind-merge";
+import { 
+  useEffect,
+  ChangeEvent, 
+  useState, 
+  useMemo,
+} from "react"
+import Button from "@/components/button"
+import Table from "@/components/table"
+import Input from "@/components/input"
+import { AmountIcon } from "@/assets/icons/amount"
+import { twMerge } from "tailwind-merge"
+import { GET_MAX_INTEREST_RATE, GET_USDT_SUPPLY_TRANSACTION } from "@/constants/query"
+import { queryClient } from "@/wagmi"
+import { useToast } from "@/hooks/useToast"
+import { ERROR_MESSAGE, FAILED_WALLET_CONNECTION, SUCCESS_OPERATION, WALLET_INSTALL, WARNING_MESSAGE } from "@/constants/message"
+import { TxClient } from "@/cf-client/client"
+import { MsgRequestSupplyUsdt } from "@/cf-client/cfprotocol.lock/tx"
+import { TailSpin } from "react-loader-spinner"
+import { useMaxInterestRate } from "@/hooks/queries/useMaxInterestRate"
 import {
-	GET_MAX_INTEREST_RATE,
-	GET_USDT_SUPPLY_TRANSACTION,
-} from "@/constants/query";
-import { queryClient } from "@/wagmi";
-import { useToast } from "@/hooks/useToast";
-import {
-	ERROR_MESSAGE,
-	FAILED_WALLET_CONNECTION,
-	SUCCESS_OPERATION,
-	WALLET_INSTALL,
-	WARNING_MESSAGE,
-} from "@/constants/message";
-import { TxClient } from "@/cf-client/client";
-import { MsgRequestSupplyUsdt } from "@/cf-client/cfprotocol.lock/tx";
-import { TailSpin } from "react-loader-spinner";
-import { useMaxInterestRate } from "@/hooks/queries/useMaxInterestRate";
-import {
-	useConnect as wagmiUseConnect,
-	useAccount as wagmiUseAccount,
-} from "wagmi";
-import { useWeb3Context } from "@/contexts/web3";
-import { useAccount, useOfflineSigners } from "graz";
-import { IPool } from "@/types/api/pool";
-import { toWei } from "@/utils";
-import { useAssetProfile } from "@/hooks/queries/useAssetProfile";
-import BigNumber from "bignumber.js";
+  useConnect as wagmiUseConnect,
+  useAccount as wagmiUseAccount
+} from "wagmi"
+import { useWeb3Context } from "@/contexts/web3"
+import { useAccount, useOfflineSigners } from "graz"
+import { IPool } from "@/types/api/pool"
+import { toWei } from "@/utils"
+import { useAssetProfile } from "@/hooks/queries/useAssetProfile"
 
 export interface Props {
-	data: IPool;
+  data: IPool
 }
 
 export const SupplyUSDTContainer = (props: Props) => {
-	const { messageApi } = useToast();
-
-	// ether metamask
-	const { data: account } = useAccount();
-	const { data: offlineSigners } = useOfflineSigners();
+  const { messageApi } = useToast()
+  
+  // ether metamask
+  const { data: account } = useAccount()
+  const { data: offlineSigners } = useOfflineSigners()
 	const { address, connector, isConnected } = wagmiUseAccount();
 	const { connectors } = wagmiUseConnect();
 
-	const { getTokenBalance, approveUSDT } = useWeb3Context();
+  const { 
+    getTokenBalance,
+    approveUSDT,  
+  } = useWeb3Context()
 
-	const [balance, setBalance] = useState<number | undefined>(undefined);
-	const [amount, setAmount] = useState<number | undefined>(undefined);
-	const [rate, setRate] = useState<number | undefined>(undefined);
-	const [loading, setLoading] = useState<boolean>(false);
-	const { data: maxRate } = useMaxInterestRate();
-	const { data: assetProfiles } = useAssetProfile();
-
-	const _is_connected_metamask =
+  const [ balance, setBalance ] = useState<number | undefined>(undefined)
+  const [ amount, setAmount ] = useState<number | undefined>(undefined)
+  const [ rate, setRate ] = useState<number | undefined>(undefined)
+  const [ loading, setLoading ] = useState<boolean>(false)
+  const { data: maxRate } = useMaxInterestRate()
+  const { data: assetProfiles } = useAssetProfile()
+  
+  const _is_connected_metamask =
 		(address && isConnected && connector === connectors[0]) ?? false;
 
-	/**
+  /**
 	 * Get decimal of collateral asset
 	 */
 	const getDecimal = useMemo(() => {
-		return (
-			assetProfiles?.find((e) => e.symbol === props.data.asset_symbol)
-				?.decimals ?? 0
-		);
-	}, [assetProfiles]);
+		return assetProfiles?.find(e => e.symbol === props.data.asset_symbol)?.decimals ?? 0
+	}, [assetProfiles])
 
 	/**
 	 * Handle collateral amount by symbol's decimal
 	 */
 	const handleAmountUpdate = (e: ChangeEvent<HTMLInputElement>) => {
-		if (e.target.value === "") return;
-		const regex = new RegExp(`^\\d*\\.?\\d{0,${getDecimal}}$`);
-		if (regex.test(e.target.value.toString())) {
-			setAmount(parseFloat(e.target.value));
-		}
-	};
+		if (e.target.value === '')
+      return 
+    const regex = new RegExp(`^\\d*\\.?\\d{0,${getDecimal}}$`)
+    if (regex.test(e.target.value.toString())) {
+      setAmount(parseFloat(e.target.value))
+    }
+	}
 
-	/**
-	 * Handle supply
-	 */
-	const handleSupply = async () => {
-		if (!window.keplr) {
-			return messageApi.Alert(WALLET_INSTALL("Kelpr"));
-		}
-		if (!account?.bech32Address || !offlineSigners?.offlineSigner) {
-			return messageApi.Alert(FAILED_WALLET_CONNECTION(`Kelpr`));
-		}
+  /**
+   * Handle supply
+   */
+  const handleSupply = async () => {
 
-		if (!address || !_is_connected_metamask)
-			return messageApi.Alert(FAILED_WALLET_CONNECTION("Metamask"));
+    if (!window.keplr) {
+      return messageApi.Alert(WALLET_INSTALL("Kelpr"));
+    }
+    if (!account?.bech32Address || !offlineSigners?.offlineSigner) {
+      return messageApi.Alert(FAILED_WALLET_CONNECTION(`Kelpr`));
+    }
 
-		if (!amount || amount <= 0) {
-			return messageApi.Alert({ ...WARNING_MESSAGE });
-		}
+    if (!address || !_is_connected_metamask)
+      return messageApi.Alert(FAILED_WALLET_CONNECTION('Metamask'));
 
-		if (!rate || (rate && rate > Number(maxRate?.max_interest_rate ?? 0) * 100))
-			return;
+    if (!amount || amount <= 0) {
+      return messageApi.Alert({...WARNING_MESSAGE});
+    }
 
-		try {
-			setLoading(true);
+    if (!rate || rate && rate > (Number(maxRate?.max_interest_rate ?? 0) * 100))
+      return
 
-			const approve = await approveUSDT(
-				BigNumber(amount).multipliedBy(1e18).toFixed(),
-			);
-			if (!approve) {
-				setLoading(false);
-				return;
-			}
-			const _supplyData: MsgRequestSupplyUsdt = {
-				creator: account?.bech32Address,
-				assetId: Number(props.data.asset_id),
-				chainSymbol: props.data.chain_symbol,
-				amount: toWei(amount).toString(),
-				interestRate: (rate / 100).toString(),
-				senderAddress: address,
-			};
+    try {
+      setLoading(true)
+
+      const approve = await approveUSDT((amount * 1e18).toString())
+      if (!approve) {
+        setLoading(false)
+        return
+      }
+      const _supplyData: MsgRequestSupplyUsdt = {
+        creator: account?.bech32Address,
+        assetId: Number(props.data.asset_id),
+        chainSymbol: props.data.chain_symbol,
+        amount: toWei(amount).toString(),
+        interestRate: (rate / 100).toString(),
+        senderAddress: address
+      }
 
       const client = await TxClient(offlineSigners?.offlineSigner);
       let msg = await client.msgRequestSupplyUSDT(_supplyData);
       const { data, error } = await client.signAndBroadcast([msg]);
-      
+
       if (!data || !!error) {
         setLoading(false)
         messageApi.Alert(ERROR_MESSAGE(error as string))
         return
       }
-
+      
       await invalidateQuery()
 
-			messageApi.Alert(SUCCESS_OPERATION("Successfully supplied USDT."));
+      setLoading(false)
 
-			await queryClient.invalidateQueries({
-				queryKey: [GET_USDT_SUPPLY_TRANSACTION],
-			});
-		} catch (error) {
-			setLoading(false);
-			messageApi.Alert(ERROR_MESSAGE(error as string));
-		}
-	};
+      messageApi.Alert(SUCCESS_OPERATION('Successfully supplied USDT.'))
 
-	/**
-	 * Invalidate queries
-	 */
-	const invalidateQuery = async () => {
-		Promise.all([
-			queryClient.invalidateQueries({ queryKey: [GET_MAX_INTEREST_RATE] }),
-		]);
-	};
+      await queryClient.invalidateQueries({
+        queryKey: [GET_USDT_SUPPLY_TRANSACTION],
+      })
+    } catch (error) {
+      setLoading(false)
+      messageApi.Alert(ERROR_MESSAGE(error as string))
+    }
+  }
 
-	// get balance in holesky network
-	useMemo(async () => {
-		const _balance = await getTokenBalance();
-		setBalance(_balance);
-	}, [address]);
 
-	useEffect(() => {
-		invalidateQuery();
-	}, []);
+  /**
+   * Invalidate queries
+   */
+  const invalidateQuery = async () => {
+    Promise.all([
+      queryClient.invalidateQueries({ queryKey: [GET_MAX_INTEREST_RATE] }),
+    ])
+  }
 
-	return (
-		<div className="w-full mt-[30px]">
-			{/* USDT amount to supply */}
-			<Input.Number
-				label="Input USDT amount to supply"
-				value={amount ?? ""}
-				placeholder="0"
-				icon={<AmountIcon />}
-				innerButtonLabel="Max"
-				errorMsg={
-					amount && amount > (balance || 0)
-						? `Amount should be less than ${balance}`
-						: null
-				}
-				onMax={() => setAmount(balance ?? 0)}
-				onChange={(e: ChangeEvent<HTMLInputElement>) => handleAmountUpdate(e)}
-				classOverride={{
-					container: "mt-8",
-					inputContainer: "bg-black mt-3",
-					input: "bg-black ml-1",
-					value: "text-[13px] text-[#5e7e8e]",
-					icon: "w-8",
-				}}
-			/>
+  // get balance in holesky network
+  useMemo(async () => {
+    const _balance = await getTokenBalance()
+    setBalance(_balance)
+  }, [address])
 
-			<Input.Number
-				label="Interest Rate ( % )"
-				value={rate ?? ""}
-				placeholder="0"
-				icon={<AmountIcon />}
-				innerButtonLabel="Max"
-				errorMsg={
-					rate && rate > Number(maxRate?.max_interest_rate ?? 0) * 100
-						? `Interest rate should be less than ${Number((Number(maxRate?.max_interest_rate ?? 0) * 100).toFixed(0))} %`
-						: null
-				}
-				onMax={() =>
-					setRate(
-						Number((Number(maxRate?.max_interest_rate ?? 0) * 100).toFixed(0)),
-					)
-				}
-				onChange={(e: ChangeEvent<HTMLInputElement>) =>
-					setRate(parseFloat(e.target.value))
-				}
-				classOverride={{
-					container: "mt-6",
-					inputContainer: "bg-black mt-3",
-					input: "bg-black ml-1",
-					value: "text-[13px] text-[#5e7e8e]",
-					icon: "w-8",
-				}}
-			/>
+  useEffect(() => {
+    invalidateQuery()
+  }, [])
 
-			{/* Button group */}
-			<div className="flex flex-col gap-6 pt-6">
-				{loading ? (
-					<div className="flex flex-1 justify-center items-center bg-[#0aab8b] rounded-lg py-[17px]">
-						<TailSpin
-							visible={true}
-							height="20"
-							width="20"
-							color="#fff"
-							ariaLabel="tail-spin-loading"
-							wrapperStyle={{}}
-							wrapperClass=""
-						/>
-					</div>
-				) : Number(amount) <= 0 ? (
-					<>
-						<div className="flex flex-col gap-2.5 mt-8 ">
-							<Button.Basic
-								label="Enter valid amount"
-								className={twMerge(
-									"w-full bg-[#36f5cf]/10",
-									// 'hover:bg-[#0aab8b]'
-								)}
-								onClick={() => {}}
-							/>
-						</div>
-					</>
-				) : (
-					<Button.Basic
-						label="Supply"
-						className={twMerge(
-							"w-full bg-[#36f5cf]/10",
-							// inscribed && 'bg-[#0aab8b]',
-						)}
-						onClick={handleSupply}
-					/>
-				)}
-			</div>
+  return (
+    <div className="w-full mt-[30px]">
 
-			{/* Locked Table */}
-			<Table.SupplyUSDTTransaction />
-		</div>
-	);
-};
+      {/* USDT amount to supply */}
+      <Input.Number 
+        label="Input USDT amount to supply"
+        value={amount ?? ''}
+        placeholder="0"
+        icon={<AmountIcon />}
+        innerButtonLabel="Max"
+        errorMsg={
+          amount && amount > (balance || 0)
+            ? `Amount should be less than ${balance}`
+            : null
+        }
+        onMax={() => setAmount(balance ?? 0)}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => 
+          handleAmountUpdate(e)
+        }
+        classOverride={{
+          container: 'mt-8',
+          inputContainer: 'bg-black mt-3',
+          input: 'bg-black ml-1',
+          value: 'text-[13px] text-[#5e7e8e]',
+          icon: 'w-8'
+        }}
+      />
+
+      <Input.Number 
+        label="Interest Rate ( % )"
+        value={rate ?? ''}
+        placeholder="0"
+        icon={<AmountIcon />}
+        innerButtonLabel="Max"
+        errorMsg={
+          rate && rate > (Number(maxRate?.max_interest_rate ?? 0) * 100) 
+            ? `Interest rate should be less than ${Number((Number(maxRate?.max_interest_rate ?? 0) * 100).toFixed(0))} %`
+            : null
+        }
+        onMax={() => setRate(Number((Number(maxRate?.max_interest_rate ?? 0) * 100).toFixed(0)))}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => 
+          setRate(parseFloat(e.target.value ))
+        }
+        classOverride={{
+          container: 'mt-6',
+          inputContainer: 'bg-black mt-3',
+          input: 'bg-black ml-1',
+          value: 'text-[13px] text-[#5e7e8e]',
+          icon: 'w-8'
+        }}
+      />
+
+      {/* Button group */}
+      <div className="flex flex-col gap-6 pt-6">
+        {loading ? (
+          <div className="flex flex-1 justify-center items-center bg-[#0aab8b] rounded-lg py-[17px]">
+            <TailSpin
+              visible={true}
+              height="20"
+              width="20"
+              color="#fff"
+              ariaLabel="tail-spin-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+          </div>
+        ) : Number(amount) <= 0 ? (
+        <>
+          <div className="flex flex-col gap-2.5 mt-8 ">
+            <Button.Basic 
+              label="Enter valid amount"
+              className={twMerge(
+                "w-full bg-[#36f5cf]/10",
+                // 'hover:bg-[#0aab8b]'
+              )}
+              onClick={() => {}}
+            />
+          </div>
+        </>
+      ) : (
+        <Button.Basic 
+          label="Supply"
+          className={twMerge(
+            "w-full bg-[#36f5cf]/10",
+            // inscribed && 'bg-[#0aab8b]',
+          )}
+          onClick={handleSupply}
+        />
+      )}
+      </div>
+
+      {/* Locked Table */}
+      <Table.SupplyUSDTTransaction />
+      
+    </div>
+  )
+}
+
